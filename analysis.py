1e14e0d5a1bf672c96f5ec4d017dcbd58eb79408
Alejo Salles <alejosalles@gmail.com>
2014-11-04 20:03:33 -0800

    setup for large scale running

--- analysis.py
+++ analysis.py
@@ -7,44 +7,70 @@ import entropy_gains
 import learners
 import world
 import numpy
+import time
+import parameters
 
 # for kid in data.get_kids():
 # 	print "Kid {0}: {1}, RandomLearner: {2}, TheoryLearner: {3}".format(kid, keg, reg, teg)
 	
 
 def main(player):
+	starttime=time.clock()
 	data=Data.Data()
 	data.read(astext=False)
-	truncate=50
+	n_kids=parameters.n_kids
+	truncate=parameters.n_truncate
+	n_r_theo=parameters.n_r_theo
+	n_r_random=parameters.n_r_random
 
-
-
+	eg=np.zeros(len(data.get_kids()[:n_kids]))
+	
 	if player=='kids':
-		for kid in data.get_kids():
+		n_r=1
+		for k,kid in enumerate(data.get_kids()[:n_kids]):
 			kidseq=data.data[kid][:truncate]
 			keg=entropy_gains.ave_theory_expected_entropy_gain(kidseq)[0]
-			print kid, keg
+			#print kid, keg
+			eg[k]=keg
 			#nkegs
 
-
-
 	elif player=='random':
-		rl=learners.RandomLearner()
-		for kid in data.get_kids():
-			rlseq=rl.play(min(data.get_kid_nactions(kid),truncate))
-			reg=entropy_gains.ave_theory_expected_entropy_gain(rlseq)[0]
-			print kid, reg
-		
+		n_r=n_r_theo
+		egall=np.zeros((len(data.get_kids()[:n_kids]), n_r))
+		for k,kid in enumerate(data.get_kids()[:n_kids]):
+			for r in range(n_r_random):
+				rl=learners.RandomLearner()
+				rlseq=rl.play(min(data.get_kid_nactions(kid),truncate))
+				reg=entropy_gains.ave_theory_expected_entropy_gain(rlseq)[0]
+				#print kid, reg
+				eg[k]+=reg
+				egall[k,r]=reg
+			eg[k]/=n_r
 		#[d.display() for d in rlseq]
 		#print reg
 
 	elif player=='theory':
-		tl=learners.TheoryLearner()
-		for kid in data.get_kids():
-			tlseq=tl.play(min(data.get_kid_nactions(kid),truncate))
-			teg=entropy_gains.ave_theory_expected_entropy_gain(tlseq)[0]
-			print kid, teg
+		n_r=n_r_theo
+		egall=np.zeros((len(data.get_kids()[:n_kids]), n_r))
+		for k,kid in enumerate(data.get_kids()[:n_kids]):
+			for r in range(n_r_theo):
+				tl=learners.TheoryLearner()
+				tlseq=tl.play(min(data.get_kid_nactions(kid),truncate))
+				teg=entropy_gains.ave_theory_expected_entropy_gain(tlseq)[0]
+				#print kid, teg
+				eg[k]+=teg
+				egall[k,r]=teg
+			eg[k]/=n_r
 
+
+	filename='Output/out-'+player+'-'+str(truncate)+'_tru-'+str(n_r)+'_real.txt'
+	np.savetxt(filename, eg)
+
+	if player in ['random', 'theory']:
+		filenameall='Output/all-'+player+'-'+str(truncate)+'_tru-'+str(n_r)+'_real.txt'
+		np.savetxt(filenameall, egall)
+	
+	print 'time elapsed for run {0}: {1:.0f} s'.format(filename, time.clock()-starttime)
 
 
 
