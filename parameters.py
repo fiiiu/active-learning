c92bd233c2bd51c5accee07b5ef3ef33e5b19572
Alejo Salles <alejosalles@gmail.com>
2014-11-07 19:31:52 -0800

    changed order dirs in parameters.py to make it more comfortable to run

--- parameters.py
+++ parameters.py
@@ -1,7 +1,8 @@
 
-directory='/Users/alejo/Neuro/ActiveLearning/'
+#directory='/Users/alejo/Neuro/ActiveLearning/'
+directory='/home/alejo/Run/active-learning/'
+
 output_directory=directory+'Output/'
-#directory='/home/alejo/Run/active-learning'
 n_kids=3#29 #31 total, use 40
 #truncate=75
 n_r_theo=1#5
