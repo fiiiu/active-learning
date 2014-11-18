05ffdc4330b1ef81b9f7644ba8e66f679389c452
Alejo Salles <alejosalles@gmail.com>
2014-11-17 19:02:36 -0800

    fixed epsilon and setup for remote run

--- parameters.py
+++ parameters.py
@@ -1,12 +1,12 @@
 
 directory='/Users/alejo/Neuro/ActiveLearning/'
-directory='/home/alejo/Run/active-learning/'
+#directory='/home/alejo/Run/active-learning/'
 
 output_directory=directory+'Output/'
 
-n_kids=31#29 #31 total, use 40
+n_kids=1#29 #31 total, use 40
 #truncate=75
 #n_r_theo=1#5
-n_r_random=20#20#0#50
+n_r_random=1#20#20#0#50
 
 epsilon=1e-3
